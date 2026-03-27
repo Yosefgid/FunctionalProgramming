c892972672d7714354415932e69923ffca3e5c73
Yosefgid <jossykasshun@yahoo.com>
2026-03-27 16:29:25 +0000

    Remove commented out code

--- FunctionalProgramming.Tests/CheckValidEmailTest.cs
+++ FunctionalProgramming.Tests/CheckValidEmailTest.cs
@@ -12,17 +12,7 @@ public class CheckValidEmailTest
 //        result.ShouldBe(Exercises001.ifValid);
 
 
-//    }
-
-//    [Test]
-//    public void CheckValidEmail_ReturnsifnotValidMessage_When_EmailInvalid()
-//    {
-//        string email = "yos@northcoders.co.uk";
-//        string result = Exercises001.CheckValidEmail(email);
-//        result.ShouldBe(Exercises001.ifValid);
-//        //This is an Red becuase we wanted it to fail making sure we do not get any false positive
-
-//    }
+//   
 
     [TestCase("MarkPiler@northcoders.co.uk")]
     [TestCase("yos@northcoders.co.uk")]
